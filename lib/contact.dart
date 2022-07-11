ffb68926a703f665f5d1d0eef5333a1f38640184
Davi Jorge <guimell11@hotmail.com>
2022-07-11 01:25:15 -0300

    add textfield and send button

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'widgets.dart';
 import 'config.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
 
+  @override
+  State<ContactPage> createState() => ContactPageState();
+}
+
+class ContactPageState extends State<ContactPage> {
   @override
   Widget build(BuildContext context) {
     SiteConfig.screenHeight = MediaQuery.of(context).size.height;
@@ -13,14 +19,68 @@ class ContactPage extends StatelessWidget {
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(20),
-              ),
-              child: Container(
-                height: SiteConfig.screenHeight * 0.5,
-                width: SiteConfig.screenHeight * 0.5,
-                color: Colors.amber,
+            child: SizedBox(
+              height: SiteConfig.screenHeight * 0.8,
+              width: SiteConfig.screenHeight * 0.8,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color:
+                    MediaQuery.platformBrightnessOf(context) == Brightness.light
+                        ? Colors.white
+                        : Colors.black,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Contato",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: MediaQuery.platformBrightnessOf(context) ==
+                                      Brightness.light
+                                  ? Colors.black
+                                  : Colors.white),
+                        ),
+                      ),
+                      const MyTextField(
+                        labelText: 'Nome :',
+                      ),
+                      const MyTextField(
+                        labelText: 'Sobrenome :',
+                      ),
+                      const MyTextField(
+                        labelText: 'Nome da empresa :',
+                      ),
+                      const MyTextField(
+                        labelText: "CNPJ",
+                      ),
+                      const MyTextField(
+                        labelText: 'E-mail corporativo :',
+                      ),
+                      const MyTextField(
+                        labelText: 'Quantidade de funcionarios :',
+                      ),
+                      const MyTextField(
+                        labelText: 'Cidade e estado da empresa :',
+                      ),
+                      const MyTextField(
+                        labelText: 'Mensagem :',
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {}, child: const Text("Enviar"))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
